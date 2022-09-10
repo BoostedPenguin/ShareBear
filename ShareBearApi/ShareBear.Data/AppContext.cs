8e872adbdfbda2d66f645d7d6833fbfbd4d56dfd
BoostedPenguin <alex20009@abv.bg>
2022-09-10 16:35:30 +0200

    Basic structure added

---
+++ ShareBearApi/ShareBear.Data/AppContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShareBear.Data
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
