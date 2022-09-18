74bcaf42ffc934e22191b3910cf2844f14cc8389
BoostedPenguin <alex20009@abv.bg>
2022-09-18 16:43:21 +0200

    Added MUI for nextjs

--- sharebear/pages/_app.tsx
+++ sharebear/pages/_app.tsx
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import theme from '../src/theme'
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
   return <Component {...pageProps} />
 }
 
