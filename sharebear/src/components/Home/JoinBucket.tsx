8c022d02901d7bf308bc95061df0b919c4b4d8f3
Aleksandar Todorov <60142028+BoostedPenguin@users.noreply.github.com>
2022-09-24 12:36:28 +0200

    Fixing bucket code inputs

--- sharebear/src/components/Home/JoinBucket.tsx
+++ sharebear/src/components/Home/JoinBucket.tsx
@@ -5,7 +5,7 @@ import { useEffect, useRef, useState } from "react"
 
 
 export default function JoinBucket() {
-    const itemsRef = useRef<Array<HTMLDivElement | null>>([])
+    const itemsRef = useRef<Array<HTMLInputElement | null>>([])
     const [code, setCode] = useState<string>()
 
 
@@ -72,23 +72,34 @@ export default function JoinBucket() {
                             zIndex: 50
                         }} mb={5}>
                             {[...Array(6)].map((item, i) => (
-                                <input onKeyDown={(e: any) => {
-                                    if (e.key == "e" || e.key == "E") {
-                                        e.preventDefault()
-                                        return
-                                    }
+                                <input
+                                    onInput={(e: any) => e.target.value = e.target.value.slice(0, 1)}
+                                    onKeyDown={(e: any) => {
+                                        if (e.key == "e" || e.key == "E") {
+                                            e.preventDefault()
+                                            return
+                                        }
 
-                                    if (e.code == "Backspace" || e.key == "Backspace") {
-                                        e.preventDefault()
-                                        e.target.value = ""
-                                        itemsRef.current[i - 1]?.focus()
-                                    }
-                                }} onChange={(e) => {
-                                    if (e.target.value)
-                                        itemsRef.current[i + 1]?.focus()
-                                    else
-                                        e.target.value = ""
-                                }} ref={el => itemsRef.current[i] = el} key={i} type="number" maxLength={1} className={styles.customInputBox} />
+                                        if (e.code == "Backspace" || e.key == "Backspace") {
+                                            e.preventDefault()
+                                            if (e.target.value == "") {
+                                                const previousItem = itemsRef.current[i - 1]
+                                                if (previousItem)
+                                                    previousItem.value = ""
+
+                                                itemsRef.current[i - 1]?.focus()
+                                                return
+                                            }
+                                            e.target.value = ""
+                                            itemsRef.current[i - 1]?.focus()
+                                        }
+
+                                        if (e.target.value)
+                                            itemsRef.current[i + 1]?.focus()
+                                        else
+                                            e.target.value = ""
+
+                                    }} ref={el => itemsRef.current[i] = el} key={i} type="number" maxLength={1} className={styles.customInputBox} />
                             ))}
                         </Box>
                     </Stack>
